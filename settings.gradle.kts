72da7330543fd804d7d99eee1c30d93002ad0e45
Daksh <daksh@technowolf.in>
2024-10-23 02:37:45 +0530

    - Upgrade kordex gradle plugins - Fix breaking changes with new kordex version - Also introduce new dep to generate docs, However that did not work.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,15 @@ pluginManagement {
         id("com.github.johnrengelman.shadow") version "8.1.1"
 
         id("dev.kordex.gradle.docker") version "1.5.2"
-        id("dev.kordex.gradle.kordex") version "1.4.2"
+        id("dev.kordex.gradle.kordex") version "1.5.2"
+    }
+    repositories{
+        gradlePluginPortal()
+        mavenCentral()
+
+        maven("https://snapshots-repo.kordex.dev")
+        maven("https://releases-repo.kordex.dev")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
